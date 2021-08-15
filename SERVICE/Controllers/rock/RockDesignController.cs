f12069793cf0cf0a5c4ca9dcf039bf6c2408fe47
chenxiaofei <chenfei@csii.com.cn>
2021-08-15 17:34:25 +0800

    新建点提交

--- SERVICE/Controllers/rock/RockDesignController.cs
+++ SERVICE/Controllers/rock/RockDesignController.cs
@@ -31,27 +31,12 @@ namespace SERVICE.Controllers
         {
             #region 参数
             string projectId = HttpContext.Current.Request.Form["projectId"];
-            string position = HttpContext.Current.Request.Form["position"];
-            string type = HttpContext.Current.Request.Form["type"];
-
             string name = HttpContext.Current.Request.Form["name"];
             string remarks = HttpContext.Current.Request.Form["remarks"];
-            string src = HttpContext.Current.Request.Form["src"];
-            string inclination = HttpContext.Current.Request.Form["inclination"];
-            string dipAngle = HttpContext.Current.Request.Form["dipAngle"];
-            string trend = HttpContext.Current.Request.Form["trend"];
-            string traceLength = HttpContext.Current.Request.Form["traceLength"];
-            string avgOpening = HttpContext.Current.Request.Form["avgOpening"];
-            string creatTime = HttpContext.Current.Request.Form["creatTime"];
-            string modleTime = HttpContext.Current.Request.Form["modleTime"];
-            string windowId = HttpContext.Current.Request.Form["windowId"];
-            string measure = HttpContext.Current.Request.Form["measure"];
-            string modleId = HttpContext.Current.Request.Form["modleId"];
-            string collector = HttpContext.Current.Request.Form["collector"];
-
-            string colour = HttpContext.Current.Request.Form["colour"];//颜色
-            string lineType = HttpContext.Current.Request.Form["lineType"];//线的类型
-            string lineSize = HttpContext.Current.Request.Form["lineSize"];//大小
+            string profilePostion = HttpContext.Current.Request.Form["profilePostion"];
+            string drillHolePostion = HttpContext.Current.Request.Form["drillHolePostion"];
+            string measurWindowPostion = HttpContext.Current.Request.Form["measurWindowPostion"];
+            string probeSlotPostion = HttpContext.Current.Request.Form["probeSlotPostion"];
 
 
 
@@ -63,13 +48,8 @@ namespace SERVICE.Controllers
             User user = null;
             COM.CookieHelper.CookieResult cookieResult = ManageHelper.ValidateCookie(pgsqlConnection, HttpContext.Current.Request.Form["cookie"], ref user);
             #endregion
-            if(type == "1" || type =="3"|| type == "4"){//结构面，节理
-                name = PostgresqlHelper.QueryData(pgsqlConnection, string.Format("select  max(id) from rock_data_point "));
-            }
-            if (string.IsNullOrEmpty(name))
-            {
-                name = "1";
-            }
+
+
             if (cookieResult == COM.CookieHelper.CookieResult.SuccessCookkie)
             {
                 if (user == null)
@@ -78,114 +58,50 @@ namespace SERVICE.Controllers
                 }
 
                 if (!string.IsNullOrEmpty(projectId)
-                    && !string.IsNullOrEmpty(position)
-                    && !string.IsNullOrEmpty(type)
+                    && !string.IsNullOrEmpty(profilePostion)
                     && !string.IsNullOrEmpty(name)
                     && !string.IsNullOrEmpty(remarks))
                 {
-                    
+
                     if (true)
                     {
-                        string sql = "INSERT INTO rock_data_point(project_id, position, type, name, remarks" ; 
+                        string sql = "INSERT INTO rock_design_data(project_id,profile_position , name, remarks";
                         string value = "("
                         + projectId + ","
-                        + SQLHelper.UpdateString(position) + ","
-                        + SQLHelper.UpdateString(type) + ","
+                        + SQLHelper.UpdateString(profilePostion) + ","
                         + SQLHelper.UpdateString(name) + ","
                         + SQLHelper.UpdateString(remarks);
 
-                        if (!string.IsNullOrEmpty(src))
+                        if (!string.IsNullOrEmpty(drillHolePostion))
                         {
-                            sql = sql + ", src";
-                            value = value + "," + SQLHelper.UpdateString(src);
+                            sql = sql + ", drill_hole_position";
+                            value = value + "," + SQLHelper.UpdateString(drillHolePostion);
                         }
-                        if (!string.IsNullOrEmpty(inclination))
+                        if (!string.IsNullOrEmpty(measurWindowPostion))
                         {
-                            sql = sql + ", inclination";
-                            value = value + "," + SQLHelper.UpdateString(inclination);
+                            sql = sql + ", measur_window_position";
+                            value = value + "," + SQLHelper.UpdateString(measurWindowPostion);
                         }
-                        if (!string.IsNullOrEmpty(dipAngle))
+                        if (!string.IsNullOrEmpty(probeSlotPostion))
                         {
-                            sql = sql + ", dip_angle";
-                            value = value + "," + SQLHelper.UpdateString(dipAngle);
-                        }
-                        if (!string.IsNullOrEmpty(trend))
-                        {
-                            sql = sql + ", trend";
-                            value = value + "," + SQLHelper.UpdateString(trend);
-                        }
-                        if (!string.IsNullOrEmpty(traceLength))
-                        {
-                            sql = sql + ", trace_length";
-                            value = value + "," + SQLHelper.UpdateString(traceLength);
-                        }
-                        if (!string.IsNullOrEmpty(avgOpening))
-                        {
-                            sql = sql + ", avg_opening";
-                            value = value + "," + SQLHelper.UpdateString(avgOpening);
-                        }
-                        if (!string.IsNullOrEmpty(creatTime))
-                        {
-                            sql = sql + ", creat_time";
-                            value = value + "," + SQLHelper.UpdateString(creatTime);
-                        }
-                        if (!string.IsNullOrEmpty(modleTime))
-                        {
-                            sql = sql + ", modle_time";
-                            value = value + "," + SQLHelper.UpdateString(modleTime);
-                        }
-                        if (!string.IsNullOrEmpty(windowId))
-                        {
-                            sql = sql + ", window_id";
-                            value = value + "," + SQLHelper.UpdateString(windowId);
-                        }
-                        if (!string.IsNullOrEmpty(measure))
-                        {
-                            sql = sql + ", measure";
-                            value = value + "," + SQLHelper.UpdateString(measure);
-                        }
-                        if (!string.IsNullOrEmpty(modleId))
-                        {
-                            sql = sql + ", modle_id";
-                            value = value + "," + SQLHelper.UpdateString(modleId);
-                        }
-                        if (!string.IsNullOrEmpty(user.UserName))
-                        {
-                            sql = sql + ", collector";
-                            value = value + "," + SQLHelper.UpdateString(user.UserName);
-                        }
-                        if (!string.IsNullOrEmpty(colour))//颜色
-                        {
-                            sql = sql + ", colour";
-                            value = value + "," + SQLHelper.UpdateString(colour);
-                        }
-                        if (!string.IsNullOrEmpty(lineType))//线性
-                        {
-                            sql = sql + ", line_type";
-                            value = value + "," + SQLHelper.UpdateString(lineType);
-                        }
-
-                        if (!string.IsNullOrEmpty(lineSize))//线大小
-                        {
-                            sql = sql + ", line_size";
-                            value = value + "," + SQLHelper.UpdateString(lineSize);
+                            sql = sql + ", probe_slot_position";
+                            value = value + "," + SQLHelper.UpdateString(probeSlotPostion);
                         }
 
 
-
-                        sql = sql+  ") VALUES"+ value + ")";
+                        sql = sql + ") VALUES" + value + ")";
 
                         int id = PostgresqlHelper.InsertDataReturnID(pgsqlConnection, sql);
                         if (id != -1)
                         {
-                            return id+"";
+                            return id + "";
                         }
                         else
                         {
                             return "保存失败！";
                         }
                     }
-                    
+
                 }
                 else
                 {
